34831504071b340efeceef7a8f8e6567515dbca5
GranJonaHSLU <jonas.grand@stud.hslu.ch>
2020-09-23 09:56:04 +0200

    datatypeSizes

---
+++ 30_Exercises/SW02/datatypesSizes.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main( int argc, char* argv[]){
+	
+	printf("Size of Char is :%ld\n", sizeof(char));
+	printf("Size of Short is :%ld\n", sizeof(short));
+	printf("Size of Int is :%ld\n", sizeof(int));
+	printf("Size of Long is :%ld\n", sizeof(long));
+	printf("Size of Long Long is :%ld\n", sizeof(long long));
+	printf("Size of Float is :%ld\n", sizeof(float));
+	printf("Size of Double is :%ld\n", sizeof(double));
+	printf("Size of Long Double is :%ld\n", sizeof(long double));
+
+	
+	return 0;
+	
+}	
